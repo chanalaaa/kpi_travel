3d1b9f5b079ca0f2e14c49db0bb9a59c6b35b118
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-19 19:31:56 +0700

    owl carousal [+] dots, nav

--- js/main.js
+++ js/main.js
@@ -3,16 +3,10 @@ jQuery(document).ready(function($) {
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
-        nav: false,
+       navContainer: '#customNav',
         responsive: {
             0: {
                 items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
             }
         }
     })
