6c486d67bb130996cfb21d15f273405970159787
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-20 20:17:34 +0700

    input & dropdown

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
     $('.owl-carousel').owlCarousel({
-       navContainer: '#customNav',
+        navContainer: '#customNav',
         responsive: {
             0: {
                 items: 1
@@ -21,9 +21,9 @@ jQuery(document).ready(function($) {
     });
 
 
-   
-
-
+    $(".dropdown-menu").on('click', 'li', function() {
+        $(this).parents('.dropdown').find('.dropdown-toggle').html($(this).text() + '<span class="caret"></span>');
+    });
 });
 
 /*
@@ -44,55 +44,55 @@ function openNavList() {
 
 
 
- // var itemTable = 1;
-    // var $table = $(".tablesaw-swipe");
-    // var lenTable = $(".tablesaw-advance-dots li").length;
-    // $prevBtn = $(".btn-micro.left");
-    // $nextBtn = $(".btn-micro.right");
+// var itemTable = 1;
+// var $table = $(".tablesaw-swipe");
+// var lenTable = $(".tablesaw-advance-dots li").length;
+// $prevBtn = $(".btn-micro.left");
+// $nextBtn = $(".btn-micro.right");
 
-    // $prevBtn.on("click", function(e) {
-    //     if (itemTable === 1 || itemTable < 1) {
-    //         return;
-    //     } else {
-    //         itemTable -= 1;
-    //         prevTable(itemTable);
-    //     }
-    //     e.preventDefault();
-    // });
+// $prevBtn.on("click", function(e) {
+//     if (itemTable === 1 || itemTable < 1) {
+//         return;
+//     } else {
+//         itemTable -= 1;
+//         prevTable(itemTable);
+//     }
+//     e.preventDefault();
+// });
 
-    // $nextBtn.on("click", function(e) {
-    //     console.log(itemTable);
-    //     if (itemTable === lenTable || itemTable > lenTable) {
-    //         return;
-    //     } else {
-    //         itemTable += 1;
-    //         nextTable(itemTable);
-    //     }
-    //     e.preventDefault();
-    // });
+// $nextBtn.on("click", function(e) {
+//     console.log(itemTable);
+//     if (itemTable === lenTable || itemTable > lenTable) {
+//         return;
+//     } else {
+//         itemTable += 1;
+//         nextTable(itemTable);
+//     }
+//     e.preventDefault();
+// });
 
-    // $table
-    //     .on("tablesawcolumns.swipetoggle", function() {
-    //         var canGoPrev = nextTable(itemTable + 1);
-    //         var canGoNext = prevTable(itemTable);
-    //     })
-    //     .on("tablesawnext.swipetoggle", function() {
-    //         alert("fe");
-    //         itemTable += 1;
-    //     })
-    //     .on("tablesawprev.swipetoggle", function() {
-    //         itemTable -= 1;
-    //     });
+// $table
+//     .on("tablesawcolumns.swipetoggle", function() {
+//         var canGoPrev = nextTable(itemTable + 1);
+//         var canGoNext = prevTable(itemTable);
+//     })
+//     .on("tablesawnext.swipetoggle", function() {
+//         alert("fe");
+//         itemTable += 1;
+//     })
+//     .on("tablesawprev.swipetoggle", function() {
+//         itemTable -= 1;
+//     });
 
 
-    // function getCoord(event, key) {
-    //     return (event.touches || event.originalEvent.touches)[0][key];
-    // }
+// function getCoord(event, key) {
+//     return (event.touches || event.originalEvent.touches)[0][key];
+// }
 
-    // function nextTable(itemTable) {
-    //     document.getElementById("captionTitle").innerHTML = "Paragraph changed!" + itemTable;
-    // }
+// function nextTable(itemTable) {
+//     document.getElementById("captionTitle").innerHTML = "Paragraph changed!" + itemTable;
+// }
 
-    // function prevTable(itemTable) {
-    //     document.getElementById("captionTitle").innerHTML = "Paragraph changed!" + itemTable;
-    // }
+// function prevTable(itemTable) {
+//     document.getElementById("captionTitle").innerHTML = "Paragraph changed!" + itemTable;
+// }
