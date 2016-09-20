623779d228d78c641267f9f91285829597b41df8
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-20 20:38:40 +0700

    bootstrap-select ref:https://silviomoreto.github.io/bootstrap-select/

--- js/main.js
+++ js/main.js
@@ -24,7 +24,13 @@ jQuery(document).ready(function($) {
     $(".dropdown-menu").on('click', 'li', function() {
         $(this).parents('.dropdown').find('.dropdown-toggle').html($(this).text() + '<span class="caret"></span>');
     });
+    $('.selectpicker').selectpicker({
+        size: 4
+    });
 });
+
+
+
 
 /*
 function openNavList() {
