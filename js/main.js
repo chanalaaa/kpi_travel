30e644341099ce528d5191659e44988eec7dd67a
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-23 17:13:15 +0700

    table fix in firefox

--- js/main.js
+++ js/main.js
@@ -59,7 +59,25 @@ jQuery(document).ready(function($) {
                 $(this).focus();
             });
     }
+    /* table fix in firefox */
+    function firefoxFix() {
+
+        if (/firefox/.test(window.navigator.userAgent.toLowerCase())) {
+
+            var tds = document.getElementsByTagName('td');
+
+            for (var index = 0; index < tds.length; index++) {
+                tds[index].innerHTML = '<div class="ff-fix">' + tds[index].innerHTML + '</div>';
+            };
+
+            var style = '<style>' + 'td { padding: 0 !important; }' + 'td:hover::before, td:hover::after { background-color: transparent !important; }' + '</style>';
+            document.head.insertAdjacentHTML('beforeEnd', style);
+
+        };
+
+    };
+
+    firefoxFix();
+
 
 });
-
-
