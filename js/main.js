722374782254b5d556c182e7f8ed3b8840b2f9bf
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-25 19:46:48 +0700

    remove js ff-fix

--- js/main.js
+++ js/main.js
@@ -59,24 +59,6 @@ jQuery(document).ready(function($) {
                 $(this).focus();
             });
     }
-    /* table fix in firefox */
-    function firefoxFix() {
-
-        if (/firefox/.test(window.navigator.userAgent.toLowerCase())) {
-
-            var tds = document.getElementsByTagName('td');
-
-            for (var index = 0; index < tds.length; index++) {
-                tds[index].innerHTML = '<div class="ff-fix">' + tds[index].innerHTML + '</div>';
-            };
-
-            var style = '<style>' + 'td { padding: 0 !important; }' + 'td:hover::before, td:hover::after { background-color: transparent !important; }' + '</style>';
-            document.head.insertAdjacentHTML('beforeEnd', style);
-
-        };
-
-    };
-    firefoxFix();
 
     /* custom style td */
     var tableHeight = $(".tablelist").height();
