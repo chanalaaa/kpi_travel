d89f447e0627cd30f5dcd2877966b06e4f8c843b
Chanala Wilangka <c.wilangka@gmail.com>
2016-09-30 15:50:54 +0700

    setDefaultYearTrip

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-    /* table carousel */
+    /* table carousel *******************************************************/
     $('.owl-carousel').owlCarousel({
         navContainer: '#customNav',
         mouseDrag: false,
@@ -24,22 +24,44 @@ jQuery(document).ready(function($) {
             $this.trigger('prev.owl.carousel');
         });
     });
-    /* dropdown */
+    /* dropdown *******************************************************/
     $('.selectpicker').selectpicker({
         size: 4
     });
+    /* set Year trip *******************************************************/
+    $("input#radioYear,input#radioYear_in").click(function() {
+        $(this).parents(".tab-pane").addClass('year');
+    });
+    $("input#radioRound,input#radioRound_in").click(function() {
+        $(this).parents(".tab-pane").removeClass('year');
+    });
 
-    /* datepicker */
+    /* datepicker *******************************************************/
+    /* and calculate datepicker (Year trip) */
+    var start = moment();
+    function setDefaultYearTrip(start) {
+        var y = parseInt(start.format('YYYY'));
+        var m = parseInt(start.format('MM'));
+        var d = parseInt(start.format('DD'));
+        var dayOflastMonth = (start.subtract(1, 'month').endOf('month')).format('DD');
+        if ((d - 1) >= 1) {
+            $(".tab-pane").find("#resultYear").text((d - 1) + "/" + m + "/" + (y + 1));
+        } else {
+            $(".tab-pane").find("#resultYear").text((dayOflastMonth) + "/" + (m - 1) + "/" + (y + 1));
+        }
+    }
+
+
     $('input[name="startdate"],input[name="enddate"],input[name="startdate_in"]').daterangepicker({
-            locale: {
-                format: 'DD/MM/YYYY'
-            },
-            singleDatePicker: true,
-            showDropdowns: false,
-            opens: 'left',
+        locale: {
+            format: 'DD/MM/YYYY'
+        },
+        singleDatePicker: true,
+        showDropdowns: false,
+        opens: 'left'
 
-        });
-
+    }, setDefaultYearTrip);
+    setDefaultYearTrip(start);
     $(".ico_datestart").on("click", function() {
         $(this).parent().find('input').trigger("click");
     })
@@ -47,29 +69,23 @@ jQuery(document).ready(function($) {
         $(this).parent().find('input').trigger("click");
     })
 
-      /* calculate datepicker (Year trip) */
+
     $('input[name="startdate"],input[name="startdate_in"]').on('apply.daterangepicker', function(ev, picker) {
         //console.log("startdate: " + picker.startDate.format('YYYY-MM-DD'));
         var y = parseInt(picker.startDate.format('YYYY'));
         var m = parseInt(picker.startDate.format('MM'));
         var d = parseInt(picker.startDate.format('DD'));
         var dayOflastMonth = (picker.startDate.subtract(1, 'month').endOf('month')).format('DD');
-        if((d-1) >= 1){
-             $(this).parents(".tab-pane").find("#resultYear").text((d-1)+"/"+m+"/"+(y+1));
-        }else{
-             $(this).parents(".tab-pane").find("#resultYear").text((dayOflastMonth)+"/"+(m-1)+"/"+(y+1));
+        if ((d - 1) >= 1) {
+            $(this).parents(".tab-pane").find("#resultYear").text((d - 1) + "/" + m + "/" + (y + 1));
+        } else {
+            $(this).parents(".tab-pane").find("#resultYear").text((dayOflastMonth) + "/" + (m - 1) + "/" + (y + 1));
         }
-       
+
     });
 
-    $("input#radioYear,input#radioYear_in").click(function(){
-       $(this).parents(".tab-pane").addClass('year');   
-    });
-      $("input#radioRound,input#radioRound_in").click(function(){
-       $(this).parents(".tab-pane").removeClass('year');   
-    });
 
-    /* table hover */
+    /* table hover *******************************************************/
     if (screen.width < 500) {
         $("body").addClass("nohover");
         $("td, th")
@@ -80,7 +96,7 @@ jQuery(document).ready(function($) {
     }
 
 
-    /* custom style td */
+    /* custom style td *******************************************************/
     var tableHeight = $(".tablelist").height();
     $(".tablelist table td:nth-child(" + (3) + ")").addClass('hover');
     $(".tablelist table td:nth-child(" + (3) + ")").addClass('blueHover');
